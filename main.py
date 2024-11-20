691abdac16f47774831b6d396659f588f23be09d
Arish-Ray <arishriyon@gmail.com>
2024-11-20 17:26:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+num=random.randint(1,6)
+print('Welcome to the virtual dice app')
+if num==1:
+    print('your number is',num)
+if num==2:
+    print('your number is',num)
+if num==3:
+    print('your number is',num)
+if num==4:
+    print('your number is',num)
+if num==5:
+    print('your number is',num)
+if num==6:
+    print('your number is',num)
